f8dcf37096a35d4c8f6b235afcfd6ed285b8ee9e
juihuanchang <ray0987qaz@gmail.com>
2026-05-04 11:15:17 +0800

    新增右鍵選單

--- script/character_body_2d.gd
+++ script/character_body_2d.gd
@@ -6,13 +6,28 @@ var SPEED = 300.0
 var target_position: Vector2
 var is_wandering = false
 
+enum PopupIds {
+	SAY_HI = 100,
+	SHOW_POSITION,
+	DELETE_PLAYER,
+}
+var _last_mouse_position: Vector2
+@onready var _pm: PopupMenu = $"../PopupMenu"
+
 var move_time = 0.0	
 
 func _ready():
+	input_pickable = true
 	$WanderingTimer.timeout.connect(_on_timer_timeout)
 	$WanderingTimer.wait_time = randf_range(5.0,8.0)
 	$WanderingTimer.start()
-
+	_pm.clear()
+	_pm.add_item("say hi", PopupIds.SAY_HI)
+	_pm.add_item("show position", PopupIds.SHOW_POSITION)
+	_pm.add_separator()
+	_pm.add_item("delete player", PopupIds.DELETE_PLAYER)
+	_pm.id_pressed.connect(_on_popup_menu_id_pressed)
+	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	#if not is_on_floor():
@@ -65,3 +80,25 @@ func wandering() -> void:
 	target_position.y = clamp(next_position.y, 192.0, 576.0)
 	
 	is_wandering = true
+
+func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_RIGHT:
+		print("右鍵點到 player")
+
+		_last_mouse_position = get_global_mouse_position()
+		var mouse_pos := get_viewport().get_mouse_position()
+
+		_pm.position = mouse_pos
+		_pm.popup()
+
+func _on_popup_menu_id_pressed(id: int) -> void:
+	match id:
+		PopupIds.SAY_HI:
+			print("hi")
+
+		PopupIds.SHOW_POSITION:
+			print(_last_mouse_position)
+
+		PopupIds.DELETE_PLAYER:
+			print("delete player")
+			queue_free()
